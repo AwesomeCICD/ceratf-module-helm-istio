2b569ca012b4c5dc787a7d078b5b95346ff1c3b2
Jennings <jtreutel+github@protonmail.com>
2022-12-02 09:22:04 +0900

    Remove providers and related vars.

--- variables.tf
+++ variables.tf
@@ -1,21 +1,3 @@
-variable "cluster_ca_certificate" {
-  description = "k8s cluster CA cert"
-}
-
-variable "cluster_endpoint" {
-  description = "k8s cluster endpoint"
-}
-
-variable "cluster_name" {
-  description = "k8s cluster name"
-}
-
-variable "config_path" {
-  description = "path to kubeconfig file"
-  type        = string
-  default     = "~/.kube/config"
-}
-
 variable "namespace" {
   description = "namespace to deploy container runner to"
   type        = string
