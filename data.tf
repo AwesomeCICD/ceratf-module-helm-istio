a8e959a5d79adc6b37acac90454ee959fbb697f2
Jennings <jtreutel+github@protonmail.com>
2022-12-13 14:14:05 +0900

    Cleanup.

--- data.tf
+++
@@ -1,7 +0,0 @@
-data "http" "prometheus_manifest" {
-  url = "https://raw.githubusercontent.com/istio/istio/release-${var.istio_chart_version}/samples/addons/prometheus.yaml"
-
-  request_headers = {
-    Accept = "application/yaml"
-  }
-}
