c7ecf88fc64c031a4797481a757294e77933b300
Jennings <jtreutel+github@protonmail.com>
2023-01-31 15:33:36 +0900

    Add data sources to get region and OIDC info.

--- data.tf
+++ data.tf
@@ -1,10 +1,22 @@
 locals {
   target_domain_stringified = replace(var.target_domain, ".", "-")
+  oidc_provider_name        = trimprefix(data.aws_iam_openid_connect_provider.this_region.arn, "https://")
 }
+
+data "aws_region" "current" {}
 
 data "kubernetes_service_v1" "istio_ingress" {
   metadata {
     name      = "istio-ingress"
     namespace = var.istio_namespace
   }
+}
+
+# Get data from OIDC provider attached to EKS cluster so that we can create IRSA for cert-manager access to R53 via k8s SAs
+data "aws_eks_cluster" "this_region" {
+  name = var.cluster_name
+}
+
+data "aws_iam_openid_connect_provider" "this_region" {
+  url = data.aws_eks_cluster.this_region.identity[0].oidc[0].issuer
 }
