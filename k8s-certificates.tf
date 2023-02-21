edf4a4a2350305cc8abbf2bfbf1162204cde136b
Jennings <jtreutel+github@protonmail.com>
2023-02-21 09:33:09 +0900

    D'oh!

--- k8s-certificates.tf
+++ k8s-certificates.tf
@@ -5,7 +5,8 @@ resource "kubectl_manifest" "certmanager_letsencrypt_clusterissuer_prod" {
       istio_namespace       = var.istio_namespace,
       aws_region            = var.aws_region,
       r53_subdomain_zone_id = var.r53_subdomain_zone_id,
-      irsa_role_arn         = aws_iam_role.k8s_route53_access.arn
+      irsa_role_arn         = aws_iam_role.k8s_route53_access.arn,
+      target_domain         = var.target_domain
     }
   )
 }
@@ -17,7 +18,8 @@ resource "kubectl_manifest" "certmanager_letsencrypt_clusterissuer_staging" {
       istio_namespace       = var.istio_namespace,
       aws_region            = var.aws_region,
       r53_subdomain_zone_id = var.r53_subdomain_zone_id,
-      irsa_role_arn         = aws_iam_role.k8s_route53_access.arn
+      irsa_role_arn         = aws_iam_role.k8s_route53_access.arn,
+      target_domain         = var.target_domain
     }
   )
 }
