08b7162de2f789e863301252cbd287e728296d9e
Eddie Webb <ollitech@gmail.com>
2023-10-25 14:51:00 -0400

    include drdemo gw in tf

--- k8s-gateways.tf
+++ k8s-gateways.tf
@@ -51,6 +51,22 @@ resource "kubectl_manifest" "istio_gateway_region_demos" {
   ]
 }
 
+resource "kubectl_manifest" "istio_gateway_region_drdemo" {
+  force_new = true
+  yaml_body = templatefile(
+    "${path.module}/custom-resource/gateway/gateway-region-drdemo.yaml.tpl",
+    {
+      ingress_namespace         = var.ingress_namespace,
+      circleci_region           = var.circleci_region,
+      target_domain             = var.target_domain,
+      target_domain_stringified = local.target_domain_stringified
+    }
+  )
+  depends_on = [
+    helm_release.istio_ingress
+  ]
+}
+
 resource "kubectl_manifest" "istio_gateway_region_nexus" {
   force_new = true
   yaml_body = templatefile(
